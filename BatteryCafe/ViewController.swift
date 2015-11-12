c0e31bdc8cb23ac72a0fadf8890a944cb7e6345f
minami <s1311389@coins.tsukuba.ac.jp>
2015-11-13 02:28:37 +0900

    ボタンの配置できた

---
+++ BatteryCafe/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BatteryCafe
+//
+//  Created by minami on 11/13/15.
+//  Copyright © 2015 TeamDeNA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func didPushedSearchButton(sender: AnyObject) {
+        print("search")
+    }
+    @IBAction func didPushedSettingButton(sender: AnyObject) {
+        print("setting")
+    }
+  
+}
