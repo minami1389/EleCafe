860002fc819938f206973739af89205a25baa2d3
minami <s1311389@coins.tsukuba.ac.jp>
2015-11-13 03:13:25 +0900

    textFieldが出し入れされるようになった

--- BatteryCafe/ViewController.swift
+++ BatteryCafe/ViewController.swift
@@ -10,14 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchTextFieldOriginY: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        searchTextFieldOriginY.constant = -40
     }
 
     @IBAction func didPushedSearchButton(sender: AnyObject) {
-        print("search")
+        self.view.setNeedsUpdateConstraints()
+        if searchTextFieldOriginY.constant == 0 {
+            searchTextFieldOriginY.constant = -40
+        } else {
+            searchTextFieldOriginY.constant = 0
+        }
+        UIView.animateWithDuration(0.3, animations: { () -> Void in
+            self.view.layoutIfNeeded()
+        })
     }
     @IBAction func didPushedSettingButton(sender: AnyObject) {
         print("setting")
